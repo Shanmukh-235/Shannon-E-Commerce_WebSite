d4304d7d09c8d5249395c46ad6d6e4cbf48b09fc
unknown <shanmukhapoornachand14316@gmail.com>
2025-06-03 16:35:21 +0530

    Responsiveness added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Script for navigation bar
+const bar = document.getElementById('bar');
+const cs = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if(cs){
+    cs.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
