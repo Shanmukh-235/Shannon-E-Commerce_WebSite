c27c715ce6ab9a3cdfdbee33a54410c54a1e83e7
unknown <shanmukhapoornachand14316@gmail.com>
2025-06-06 11:26:57 +0530

    updated links in footer and formatted all the documents

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const bar = document.getElementById('bar');
-const cs = document.getElementById('close');
-const nav = document.getElementById('navbar');
+const bar = document.getElementById("bar");
+const cs = document.getElementById("close");
+const nav = document.getElementById("navbar");
 
-if(bar){
-    bar.addEventListener('click', () => {
-        nav.classList.add('active');
-    })
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
 }
-if(cs){
-    cs.addEventListener('click', () => {
-        nav.classList.remove('active');
-    })
+if (cs) {
+  cs.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
 }
